76cb7d90f4376bfd5d7c891e1be3ac4edc949fff
ASUS <ASUS@ASUS>
2024-07-30 20:09:25 +0530

    jenkins

---
+++ My_maven2/src/main/java/mavpjtnew/HelloWorld.java
@@ -0,0 +1,17 @@
+package mavpjtnew;
+
+public class HelloWorld {
+	public void display()
+	{
+		System.out.println("Helloworld");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HelloWorld obj=new HelloWorld();
+		obj.display();
+
+	}
+
+}
